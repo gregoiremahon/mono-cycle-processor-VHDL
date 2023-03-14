a7ce649c2797571c716ff4c9d4f017c066209d13
Grégoire Mahon <78791017+gregoiremahon@users.noreply.github.com>
2023-03-14 12:09:02 +0100

    test bench Processing Unit (ALU and Registers) without assertions

--- testbench_processing_unit.vhd
+++ testbench_processing_unit.vhd
@@ -1,105 +1,33 @@
-library ieee;
+Library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity TestBench_ALU_and_registers is
+Entity TB_ALU_and_registers is
 end entity;
 
-architecture arch_TestBench_ALU_and_registers of TestBench_ALU_and_registers is
-    signal RESET : std_logic;
-    signal WE, CLK : std_logic := '0';
-    signal OP : std_logic_vector(1 downto 0);
-    signal RW, RA, RB : std_logic_vector(3 downto 0);
-    signal busW : std_logic_vector(31 downto 0);
-    constant CLOCK_PERIOD : time := 10 ns;
+architecture arch_TB_ALU_and_registers of TB_ALU_and_registers is
+signal 	RESET : std_logic;
+signal  WE,CLK : std_logic :='0';
+signal	OP : std_logic_vector(1 downto 0);
+signal	RW,RA,RB : std_logic_vector(3 downto 0);
+signal  busW : std_logic_vector(31 downto 0);
 begin
-    -- Instantiate the DUT
-    DUT: entity work.Processing_Unit
-    port map (
-        WE => WE,
-        RESET => RESET,
-        CLK => CLK,
-        OP => OP,
-        RW => RW,
-        RA => RA,
-        RB => RB,
-        busW => busW
-    );
+--INIT:
+RESET<='0';
+CLK<='1' after 100 ps when CLK='0' else '0' after 100 ps when CLK='1';
 
-    -- Clock generator process
-    clk_gen: process
-    begin
-        while true loop
-            CLK <= '0';
-            wait for CLOCK_PERIOD / 2;
-            CLK <= '1';
-            wait for CLOCK_PERIOD / 2;
-        end loop;
-    end process;
+--	Y=A	Y=A+B		 	Y=A+B			Y=A-B			Y=A-B
+OP<=	"11",	"00" after 5 ns,	"00" after 10 ns,	"10" after 15 ns,	"10" after 20 ns;
+--	A=R(15)	A=R(1)			A=R(1)			A=R(1)			A=R(7)
+RA<=	"1111",	"0001" after 5 ns,	"0001" after 10 ns,	"0001" after 15 ns,	"0111" after 20 ns;
+--	B=R(0)	B=R(15)			B=R(15)			B=R(15)			B=R(15)
+RB<=	"0000",	"1111" after 5 ns,	"1111" after 10 ns,	"1111" after 15 ns,	"1111" after 20 ns;
+--	W=R(1)	W=R(1)			W=R(2)			W=R(3)			W=R(5)
+RW<=	"0001",	"0001" after 5 ns,	"0010" after 10 ns,	"0011" after 15 ns,	"0101" after 20 ns;
 
-    -- Reset process
-    reset_proc: process
-    begin
-        RESET <= '1';
-        wait for 100 ns;
-        RESET <= '0';
-        wait;
-    end process;
 
-    -- Testcase 1 : Y=A
-    tc1: process
-    begin
-        -- Set input values
-        WE <= '1'; -- write enable
-        OP <= "11"; -- Y = A
-        RW <= "0001"; 
-        RA <= "1111";
-        RB <= "0000";
-        wait for 10 ns;
-        WE <= '0';
+WE<='1' after 5 ns when WE='0' else '0' after 150 ps when WE='1';
 
-        -- Check output value
-        wait for 1 ns;
-        assert busW = X"0000000F" report "Testcase 1 failed" severity error;
+E0:Entity work.Processing_Unit port map(WE,RESET,CLK,OP,RW,RA,RB,busW);
 
-        -- Wait a few cycles before starting the next testcase
-        wait for 10 ns;
-    end process;
-
-    -- Testcase 2 : Y=A+B
-    tc2: process
-    begin
-        -- Set input values
-        WE <= '1';
-        OP <= "00";
-        RW <= "0001";
-        RA <= "0001";
-        RB <= "1111";
-        wait for 15 ns;
-        WE <= '0';
-
-        -- Check output value
-        wait for 1 ns;
-        assert busW = X"0000F0000" report "Testcase 2 failed" severity error;
-
-        -- Wait a few cycles before starting the next testcase
-        wait for 10 ns;
-
-    tc3: process
-    begin
-        -- Set input values
-        
-        WE <= '1';
-        OP <= "00";
-        RW <= "0001";
-        RA <= "0001";
-        RB <= "1111";
-        wait for 15 ns;
-        WE <= '0';
-
-        -- Check output value
-        wait for 1 ns;
-        assert busW = X"0000F0000" report "Testcase 2 failed" severity error;
-
-    end process;
-    end architecture;
+end architecture;
