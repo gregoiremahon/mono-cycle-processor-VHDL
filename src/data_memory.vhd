b91b6e6f32537e51146bdd12dc0aa0bd697893c8
Grégoire Mahon <78791017+gregoiremahon@users.noreply.github.com>
2023-03-16 17:25:31 +0100

    Comment 'data_memory.vhd' code

--- src/data_memory.vhd
+++ src/data_memory.vhd
@@ -2,6 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- gregoiremahon & Armand Lelong 16/03/2023
+
+-- Data Memory Entity
+-- This entity represents a memory module that can store and load 64 words of 32 bits.
+-- It has the following inputs and outputs:
+-- CLK: Clock input
+-- DataIn: 32-bit data input for writing
+-- DataOut: 32-bit data output for reading
+-- Addr: 6-bit address input for both reading and writing
+-- WrEn: Write Enable signal (1 bit)
 entity data_memory is
     port (
         CLK: in std_logic;
@@ -13,11 +23,16 @@ entity data_memory is
 end entity data_memory;
 
 architecture behavior of data_memory is
+    -- Memory array type definition (64 words of 32 bits)
     type memory_array is array (63 downto 0) of std_logic_vector(31 downto 0);
 
+    -- Memory signal declaration (memory array)
     signal memory: memory_array;
 
 begin
+    -- Write process
+    -- This process is triggered by the rising edge of the CLK signal
+    -- When WrEn is '1', the DataIn value is written to the memory location specified by Addr
     process (CLK)
     begin
         if rising_edge(CLK) then
@@ -27,6 +42,9 @@ begin
         end if;
     end process;
 
+    -- Read process (combinatorial)
+    -- The DataOut output is assigned the value from the memory location specified by Addr
     DataOut <= memory(to_integer(unsigned(Addr)));
 
 end architecture behavior;
+
