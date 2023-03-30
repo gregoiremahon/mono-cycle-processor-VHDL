2c63a54a66eb0bfe2e3123190be6b77210f0bd7b
Grégoire Mahon <78791017+gregoiremahon@users.noreply.github.com>
2023-03-30 09:46:51 +0200

    Update instruction_decoder.vhd

--- src/instruction_decoder.vhd
+++ src/instruction_decoder.vhd
@@ -1,26 +1,21 @@
-library ieee;
+
+Library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity instruction_decoder is
-    port (
-        instruction: in std_logic_vector(31 downto 0);
-        PSR: in std_logic_vector(31 downto 0);
-        nPCsel: out std_logic;
-        PSR_we: out std_logic;
-        RA, RB, RW: out std_logic_vector(3 downto 0);
-        Reg_we: out std_logic;
-        RegSel: out std_logic;
-        OP: out std_logic_vector(1 downto 0);
-        UALSrc: out std_logic;
-        WrSrc: out std_logic;
-        Data_we: out std_logic
-    );
-end entity instruction_decoder;
+Entity Instruction_Decoder is
+generic(N : positive :=32);
+port(	Instruction, PSR : in std_logic_vector(N-1 downto 0);
+	--Commande:
+	nPCsel,RegWr,UALSrc,PSREn,MemWr,WrSrc,RegSel : out std_logic;
+	OP : out std_logic_vector(1 downto 0);
+	--Commande Registre:
+	Rn,Rd,Rm : out std_logic_vector(3 downto 0));	
+end entity;
 
-architecture behavior of instruction_decoder is
-    type enum_instruction is (MOV, ADDi, ADDr, CMP, LDR, STR, BAL, BLT);
-    signal instr_courante: enum_instruction;
+Architecture arch_Instruction_Decoder of Instruction_Decoder is
+    type enum_instruction is (MOV,ADDi,ADDr,CMP,LDR,STR,BAL,BLT);
+    signal instr_courante : enum_instruction;
 
 begin
     -- Process to determine the current instruction
@@ -34,12 +29,13 @@ begin
             when "111000110101" => instr_courante <= CMP;
             when "111001100001" => instr_courante <= LDR;
             when "111001100000" => instr_courante <= STR;
-            
+            when others => instr_courante <= MOV;
         end case;
         -- BAL and BLT instructions are only on 7 bits
         case instruction(31 downto 24) is
             when "11101010" => instr_courante <= BAL;
             when "10111010" => instr_courante <= BLT;
+            when others => instr_courante <= MOV;
             
         end case;
     end process;
@@ -50,116 +46,92 @@ begin
         case instr_courante is
                 -- Still need to put correct values
             when MOV =>
-                nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                nPCsel <='1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '1';
                 RegSel <= '0';
-                OP <= "00";
+                OP <= "01";
                 UALSrc <= '1';
                 WrSrc <= '0';
-                Data_we <= '0';            
+                MemWr <= '0';
 
             when ADDi =>
                 nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '1';
                 RegSel <= '0';
                 OP <= "00";
                 UALSrc <= '1';
                 WrSrc <= '0';
-                Data_we <= '0';
+                MemWr <= '0';
 
             when ADDr =>
                 nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '1';
                 RegSel <= '0';
                 OP <= "00";
-                UALSrc <= '1';
+                UALSrc <= '0';
                 WrSrc <= '0';
-                Data_we <= '0';
+                MemWr <= '0';
 
             when CMP =>
                 nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                --PSR <= '1';
+                PSREn <= '1';
+                RegWr <= '1';
                 RegSel <= '0';
-                OP <= "00";
+                OP <= "10";
                 UALSrc <= '1';
                 WrSrc <= '0';
-                Data_we <= '0';
+                MemWr <= '0';
 
             when LDR =>
-                nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                nPCsel <= '1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '1';
                 RegSel <= '0';
                 OP <= "00";
-                UALSrc <= '1';
-                WrSrc <= '0';
-                Data_we <= '0';
+                UALSrc <= '0';
+                WrSrc <= '1';
+                MemWr <= '0';
 
             when STR =>
-                nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
-                RegSel <= '0';
-                OP <= "00";
-                UALSrc <= '1';
-                WrSrc <= '0';
-                Data_we <= '0';
+                nPCsel <= '1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '0';
+                RegSel <= '1';
+                OP <= "10";
+                UALSrc <= '0';
+                WrSrc <= '1';
+                MemWr <= '1';
 
             when BAL =>
-                nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                nPCsel <= '1';
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '0';
                 RegSel <= '0';
-                OP <= "00";
-                UALSrc <= '1';
+                OP <= "--";
+                UALSrc <= '0';
                 WrSrc <= '0';
-                Data_we <= '0';
+                MemWr <= '0';
 
             when BLT =>
-                nPCsel <= '0';
-                PSR <= '0';
-                PSR_we <= '0';
-                RA <= '1';
-                RB <= '1';
-                RW <= '0';
-                Reg_we <= '1';
+                nPCsel <= PSR(0); -- First PSR bit
+                --PSR <= '0';
+                PSREn <= '0';
+                RegWr <= '0';
                 RegSel <= '0';
-                OP <= "00";
-                UALSrc <= '1';
+                OP <= "--";
+                UALSrc <= '0';
                 WrSrc <= '0';
-                Data_we <= '0';
+                MemWr <= '0';
         end case;
-    end process
-end architecture behavior;
+    end process;
+end architecture;
