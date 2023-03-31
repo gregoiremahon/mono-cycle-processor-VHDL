a04118a6ce6269ff755a8670fc7edac877430072
Grégoire Mahon <78791017+gregoiremahon@users.noreply.github.com>
2023-03-31 14:49:07 +0200

    Update processor.vhd

--- src/processor.vhd
+++ src/processor.vhd
@@ -1,3 +1,14 @@
+
+-- __  __                                   _                                                     
+--|  \/  |                                 | |                                                    
+--| \  / | ___  _ __   ___   ___ _   _  ___| | ___   _ __  _ __ ___   ___ ___  ___ ___  ___  _ __ 
+--| |\/| |/ _ \| '_ \ / _ \ / __| | | |/ __| |/ _ \ | '_ \| '__/ _ \ / __/ _ \/ __/ __|/ _ \| '__|
+--| |  | | (_) | | | | (_) | (__| |_| | (__| |  __/ | |_) | | | (_) | (_|  __/\__ \__ \ (_) | |   
+--|_|  |_|\___/|_| |_|\___/ \___|\__, |\___|_|\___| | .__/|_|  \___/ \___\___||___/___/\___/|_|   
+--                                __/ |             | |                                           
+--                               |___/              |_|                                           
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -65,5 +76,4 @@ architecture arch_processor of processor is
                 DATAOUT => DATAOUT
             );
 
-    end architecture arch_processor;
-
+end architecture arch_processor;
